22ac28aafab672d893690ad6182db2b55a5ed7c8
Suraj Kumar <91002303+ElconicsSuraj@users.noreply.github.com>
2026-01-23 13:13:33 +0530

    Refactor sensor reading and adjust intervals

--- puck_v7.js
+++ puck_v7.js
@@ -1,37 +1,40 @@
 LED1.write(0);
 LED2.write(0);
 LED3.write(0);
+
 NRF.setTxPower(-8);
-// -----------------------------------------------------
 
 var LOW_BATT_LIMIT = 25; // %
 
 function readAndSend() {
-  Puck.accelOn();
-  Puck.gyroOn();           //  ADD (enable gyro only during read)
 
+  // Enable ACC + GYRO together (CORRECT WAY)
+  Puck.accelOn({ gyro:true });
+
+  // Give IMU time to settle
   setTimeout(function () {
+
     function onAccel(a) {
 
       var temp = Math.round(E.getTemperature());
       var batt = Math.round(Puck.getBatteryPercentage());
 
       var buf = new Uint8Array(17);
-      var dv = new DataView(buf.buffer);
+      var dv  = new DataView(buf.buffer);
 
       // Header
       buf[0] = 0xAA;
       buf[1] = 0x55;
 
       // ACC
-      dv.setInt16(2,  a.acc.x|0, true);
-      dv.setInt16(4,  a.acc.y|0, true);
-      dv.setInt16(6,  a.acc.z|0, true);
+      dv.setInt16(2,  a.acc.x | 0, true);
+      dv.setInt16(4,  a.acc.y | 0, true);
+      dv.setInt16(6,  a.acc.z | 0, true);
 
-      // GYRO
-      dv.setInt16(8,  a.gyro.x|0, true);
-      dv.setInt16(10, a.gyro.y|0, true);
-      dv.setInt16(12, a.gyro.z|0, true);
+      // GYRO ✅ now works
+      dv.setInt16(8,  a.gyro.x | 0, true);
+      dv.setInt16(10, a.gyro.y | 0, true);
+      dv.setInt16(12, a.gyro.z | 0, true);
 
       // TEMP + BATT
       dv.setInt8 (14, temp);
@@ -45,25 +48,26 @@ function readAndSend() {
       // Send packet
       Bluetooth.write(buf);
 
-      // Cleanup sensors immediately
+      // Cleanup
       Puck.removeListener("accel", onAccel);
-      Puck.gyroOff();       //  ADD (important for battery)
       Puck.accelOff();
 
-      // LOW BATTERY → DEEP SLEEP
+      // Low battery → deep sleep
       if (batt < LOW_BATT_LIMIT) {
         setTimeout(function () {
-          NRF.sleep();      // ultra-low power
+          NRF.sleep();
         }, 200);
       }
     }
 
     Puck.on("accel", onAccel);
-  }, 50);
+
+  }, 200);
 }
 
 clearInterval();
-setInterval(readAndSend, 2 * 60 * 1000);
+setInterval(readAndSend, 5*60 * 1000);
 readAndSend();
 
-//save();  // RUN ONCE, THEN COMMENT
+//save();  // run once, then comment
+
